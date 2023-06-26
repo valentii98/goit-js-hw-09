d75fd2d0ecdae0eb152821c3f206e684939c33f3
Bohdan Valentii <b.valentii98@gmail.com>
2023-06-26 15:10:56 +0300

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const DELAY = 1000;
+let timerId = null;
+stopBtn.disabled = true;
+stopBtn.addEventListener('click', onStopBtn);
+startBtn.addEventListener('click', onStartBtn);
 
+function onStartBtn(e) {
+  e.target.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+}
+
+function onStopBtn(e) {
+  startBtn.disabled = false;
+  e.target.disabled = true;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
